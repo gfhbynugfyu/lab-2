7ac447a004a9d2da80d6c7bb5b2dd508aa7c6d4c
top-secret666 <chupacabra2.0666@gmail.com>
2024-12-09 22:11:06 +0300

    Добавление исходного кода проекта

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    double x,y,z,s;
+    printf ("Input x,y,z:\n");
+    scanf ("%lf%lf%lf", &x, &y, &z, "\n");
+    s= fabs(pow(x,(y/x))-cbrt((y/x)))+(y-x)*((cos(y)-(z/(y-x)))/(1+pow((y-x),2)));
+    printf("s= %.6lf",s );
+    return 0;//лабораторная работа 2
+}
 
